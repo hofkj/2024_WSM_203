e18777e621d9bc9c8408b7753fbe09a98586becb
황유진 <w2311@e-mirim.hs.kr>
2024-06-03 14:59:37 +0900

    학교 급식 API 가져오자, 이전/다음 날짜 주말 끼면 주말 빼자

--- 240513_nav/js/school-food.js
+++ 240513_nav/js/school-food.js
@@ -22,6 +22,18 @@ const displayDate = () => {
 //급식 정보 날짜 바꾸자
 const changeDate = (diff) => {
     currentDate.setDate(currentDate.getDate() + diff);
+
+    if(currentDate.getDay() === 0) {
+        currentDate.setDate(currentDate.getDate() - 2);
+    };
+    
+    if( currentDate.getDay() === 6) {
+        currentDate.setDate(currentDate.getDate() + 2);
+    }
+
+
+
+
     displayDate();
 
     const dateData = currentDate.toISOString().slice(0, 10).replace(/-/g, "");
@@ -43,8 +55,86 @@ const getSchoolFoodMenu = (dateData) => {
     //error 있다면 catch 함수 호출되고, 에러 출력하자
     fetch(url)
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => setSchoolFoodMenu(data))
     .catch((error) => console.log(error));
 }
 
 //받아온 급식 정보 웹사이트에 표시하자
+const setSchoolFoodMenu = (data) => {
+    // breakfastMenuUl 가져오자
+    //lunchMenuUl 가져오자
+    //dinnerMenuUl 가져오자
+    const breakfastMenuUl = document.getElementsByClassName("menu breakfast")[0];
+    const lunchMenuUl = document.getElementsByClassName("menu lunch")[0];
+    const dinnerMenuUl = document.getElementsByClassName("menu dinner")[0];
+    breakfastMenuUl.innerHTML = "<li>급식 메뉴를 불러오지 못했습니다.</li>"
+    lunchMenuUl.innerHTML = "<li>급식 메뉴를 불러오지 못했습니다.</li>"
+    dinnerMenuUl.innerHTML = "<li>급식 메뉴를 불러오지 못했습니다.</li>"
+
+
+    //data에서 메뉴들 가져오자(조식,중식, 석식)
+    if(data["mealServiceDietInfo"] === undefined) return;
+
+    const menuData= data["mealServiceDietInfo"][1]["row"];
+
+    //하나씩 돌면서 clean 작업하자
+    menuData.forEach((menuRow) => {
+
+        let cleanedMenu = menuRow.DDISH_NM;
+        //(...)없애자
+        cleanedMenu = cleanedMenu.replace(/\([^\)]*\)/g, "");     
+        //.없애자
+        cleanedMenu = cleanedMenu.replace(/\./g, "");       
+        //.점 문자 찾아서 "" 대체
+
+        //*없애자
+        cleanedMenu = cleanedMenu.replace(/\*/g, "");
+        //* 문자 찾아서 "" 대체
+        
+        //<br/>태그로 나누자
+        let cleanedMenuArray = cleanedMenu.split("<br/>");
+        
+        //빈칸 없애자
+        cleanedMenuArray = cleanedMenuArray.map((item) => item.trim());
+        
+        let menuFoodLis = "";
+        cleanedMenuArray.forEach((menuFood) => {
+            menuFoodLis += `<li class="menu-food">${menuFood}</li>\n`;
+        });
+        console.log(menuFoodLis);
+
+
+        //<li class="menu-food">가져온 메뉴 음식 하나씩</li>
+        if(menuRow["MMEAL_SC_NM"] === "조식") {
+            //조식의 경우, breakfastMenuUl에 넣자
+            breakfastMenuUl.innerHTML = menuFoodLis;
+        } else if(menuRow["MMEAL_SC_NM"] === "중식") {
+            //중식의 경우, lunchMenuUl에 넣자
+            lunchMenuUl.innerHTML = menuFoodLis;
+        } else if (menuRow["MMEAL_SC_NM"] === "석식") {
+            //석식의 경우, dinnerMenuUl에 넣자 js -> HTML
+            dinnerMenuUl.innerHTML = menuFoodLis;
+        }
+
+    });
+
+
+
+
+    // console.log("setSchoolFoodMenu", data["mealServiceDietInfo"]);
+    // console.log("중식 : ", data["mealServiceDietInfo"][1]["row"][1]["DDISH_NM"]);
+    // let breakfastMenu 
+    // const TEMP_JSON = {
+    //     'name' : '변우석',
+    //     'height' : '189',
+    //     'filmography' : ['선재업고튀어', '청춘기록'],
+    // };
+    // console.log(TEMP_JSON.name);
+    // console.log(TEMP_JSON["name"]);
+    // console.log(TEMP_JSON.height);
+    // console.log(TEMP_JSON.filmography);
+    // console.log(TEMP_JSON["filmography"][0]);
+
+}
+
+changeDate(0);
